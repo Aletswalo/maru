fe29ae1964c5ad9d7367aa35fa90f6d76d0d1ebb
Aletswalo <aletswalo19@gmail.com>
2024-04-21 20:25:57 +0200

    Added functions to the forecast

--- src/index.js
+++ src/index.js
@@ -50,6 +50,13 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "ac5dab4e2fef8baf42t9c2c10885f33o";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -57,23 +64,24 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    
     let forecastHtml = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
         forecastHtml = 
         forecastHtml +
     
      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" />
-    <div class="weather-forecast-icon"></div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temperature"> <strong>15&deg;</strong></span>
-    <span class="weather-forecast-temperature">9&deg;</span>
+    <div class="weather-forecast-temperature"> <strong>${Math.round(day.temperature.maximum)}&deg;</strong></div>
+    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}&deg;</div>
     </div>
     </div>
     `;
+        }
 });
 
 let forecastElement = document.querySelector("#forecast");
